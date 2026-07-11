7d6b645c88fedfc3e7ed5ffd258496b0cf4fe2d8
mubinuic <uic.mubin@gmail.com>
2026-07-11 20:57:46 +0600

    Add SEO basics: robots.txt, sitemap.xml, canonical link, JSON-LD Person schema
    Also fixes og:url/og:image/twitter:image pointing to the wrong domain
    (mubinui.github.io instead of mubinuic.github.io), which was breaking
    social link previews.

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Portfolio Website
 // Provides offline functionality and caching
 
-const CACHE_NAME = 'mubin-portfolio-v12';
+const CACHE_NAME = 'mubin-portfolio-v13';
 const urlsToCache = [
     '/',
     '/index.html',
